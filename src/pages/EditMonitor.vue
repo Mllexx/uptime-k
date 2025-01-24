fadfb18206009e4337004da3c95978d061a593b2
root <root@DESKTOP-SCOI295>
2025-01-24 17:09:07 +0300

    Change application name on views

--- src/pages/EditMonitor.vue
+++ src/pages/EditMonitor.vue
@@ -1256,7 +1256,7 @@ message HealthCheckResponse {
 <?xml version="1.0" encoding="utf-8"?>
 <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
   <soap:Body>
-    <Uptime>Kuma</Uptime>
+    <Uptime>Tracker</Uptime>
   </soap:Body>
 </soap:Envelope>` ]);
             }
